305fa6b24a93afb460470f00b4906b9fd10d1a29
ChiriemieRimando <ckgrimando@gmail.com>
2025-07-18 06:12:53 +0800

    feat: complete skills page with styles, scripts, and add new images

---
+++ pages/skills/assets/js/scripts.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const overlay = document.getElementById("skills-intro-overlay");
+
+    fetch("../../../../components/header.html")
+        .then((res) => res.text())
+        .then((data) => {
+        const header = document.getElementById("header-placeholder");
+        if (header) header.innerHTML = data;
+        })
+        .catch((err) => console.error("Header load error:", err));
+
+    if (overlay) {
+        setTimeout(() => {
+        overlay.classList.add("fade-out");
+        }, 3000);
+
+        setTimeout(() => {
+        overlay.remove();
+        document.body.classList.remove("no-scroll");
+        }, 4000);
+    }
+});
