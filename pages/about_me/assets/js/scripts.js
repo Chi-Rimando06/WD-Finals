aba707410d45866a28d9325a109ea6be1486b440
ChiriemieRimando <ckgrimando@gmail.com>
2025-07-30 14:49:05 +0800

    fix: connect git pages

--- pages/about_me/assets/js/scripts.js
+++ pages/about_me/assets/js/scripts.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
-    fetch("../../../../components/header.html")
+    fetch("/components/header.html")
         .then(response => response.text())
         .then(data => {
             const placeholder = document.getElementById("header-placeholder");
